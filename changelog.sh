78874735dc0b7a105835c94d7553d5e399403378
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-18 12:23:26 +0200

    Remove `change unrelease` command

--- changelog.sh
+++ changelog.sh
@@ -11,7 +11,6 @@ source $CHANGE/changelog-init.sh
 source $CHANGE/changelog-new.sh
 source $CHANGE/changelog-preview.sh
 source $CHANGE/changelog-release.sh
-source $CHANGE/changelog-unrelease.sh
 source $CHANGE/changelog-upgrade.sh
 
 function _changelogsh_main {
@@ -23,7 +22,6 @@ function _changelogsh_main {
   new       Register new change in the changelog
   preview   Preview the changelog in Markdown format
   release   Release the changes for the current version
-  unrelease Reverts the changes from the version specified back to unreleased
   upgrade   Pulls the latest master branch from GitHub\n"
 
   if [ "$#" -eq 0 ]; then
@@ -48,11 +46,6 @@ function _changelogsh_main {
 
   if [ $1 == 'release' ]; then
     _changelogsh_release ${@:2}
-    return
-  fi
-
-  if [ $1 == 'unrelease' ]; then
-    _changelogsh_unrelease ${@:2}
     return
   fi
 
