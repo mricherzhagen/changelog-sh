98190f99bc6bc4adb102208576fdb2058fddcc0e
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-18 12:19:40 +0200

    `change release` command updates CHANGELOG.md file

--- changelog-release.sh
+++ changelog-release.sh
@@ -14,7 +14,15 @@ function _changelogsh_release {
 
   version=$1
   expanded=$(_changelogsh_raw_to_expanded $version)
-
+ 
+  NEW_CHANGELOG=`mktemp $CHANGELOG_MKTEMP_OPTIONS`
+  if _changelogsh_preview $version > $NEW_CHANGELOG; then
+    diff $CHANGELOG_DIFF_OPTIONS "$CHANGELOG_FILENAME" "$NEW_CHANGELOG"
+    mv $NEW_CHANGELOG "$CHANGELOG_FILENAME"
+  else
+    >&2 echo "ERROR: Release aborted."
+    rm $NEW_CHANGELOG
+    exit 1
+  fi
   mv 'changelog/unreleased' "changelog/$expanded"
-  _changelogsh_preview $1
 }
