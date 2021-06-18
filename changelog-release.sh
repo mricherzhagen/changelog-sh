db2f627a45eae52b54fd99e84238ca530718a2a4
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-18 14:14:07 +0200

    `CHANGELOG_GIT_STAGE_RELEASE` option to stage changes with `change release`

--- changelog-release.sh
+++ changelog-release.sh
@@ -19,15 +19,26 @@ function _changelogsh_release {
   if _changelogsh_preview $version > $NEW_CHANGELOG; then
     diff $CHANGELOG_DIFF_OPTIONS "$CHANGELOG_FILENAME" "$NEW_CHANGELOG"
     mv $NEW_CHANGELOG "$CHANGELOG_FILENAME"
+    if [ $CHANGELOG_GIT_STAGE_RELEASE = true ]; then
+        git add "$CHANGELOG_FILENAME"
+    fi
   else
     >&2 echo "ERROR: Release aborted."
     rm $NEW_CHANGELOG
     exit 1
   fi
   if [ "$CHANGELOG_RELEASE_STRATEGY" = 'move' ]; then
-    mv 'changelog/unreleased' "changelog/$expanded"
+      if [ $CHANGELOG_GIT_STAGE_RELEASE = true ]; then
+        git mv 'changelog/unreleased' "changelog/$expanded"
+      else
+        mv 'changelog/unreleased' "changelog/$expanded"
+      fi
   elif [ "$CHANGELOG_RELEASE_STRATEGY" = 'delete' ]; then
-    rm -r changelog/unreleased
+    if [ $CHANGELOG_GIT_STAGE_RELEASE = true ]; then
+      git rm -r changelog/unreleased
+    else
+      rm -r changelog/unreleased
+    fi
   else
       >&2 echo "ERORR: \$CHANGELOG_RELEASE_STRATEGY has to be either 'move' or 'delete'. Keeping unreleased changes.";
   fi
