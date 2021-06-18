d558d228c491eb57bc41ea24bdc14e9473f536c7
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-18 11:21:35 +0200

    Always preview unreleased changes

--- changelog-preview.sh
+++ changelog-preview.sh
@@ -2,24 +2,14 @@
 
 function _changelogsh_preview {
   raw_version="Unreleased"
-  expanded="Unreleased"
   if [ "$#" -gt 0 ]; then
     raw_version=$1
-  fi
-
-  if [ $raw_version != "Unreleased" ]; then
-    expanded=$(_changelogsh_raw_to_expanded $raw_version)
-  fi
-
-  if [ ! -d "changelog/$expanded" ]; then
-    printf "$raw_version not found.\n"
-    return
   fi
 
   echo "$CHANGELOG_HEADER"
   echo "## [$raw_version]"
 
-  for dir in changelog/$expanded/*; do
+  for dir in changelog/unreleased/*; do
     if [ "$(ls -A $dir)" ]; then
       current=$(echo $dir | grep -o '[^/]*$')
       echo "###" $(_changelogsh_title $current)
