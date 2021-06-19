730ccb222f07f67c98d4ba21a9c8a2ea59bd6f75
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-19 11:32:36 +0200

    Suppress errormessage when `change` is called outside of `git` repository

--- changelog-read-conf.sh
+++ changelog-read-conf.sh
@@ -7,7 +7,7 @@ function _execute_conf_file {
 
 #Always read default config from $CHANGE
 _execute_conf_file $CHANGE/.changelog-sh-conf.sh
-if TOPLEVEL_GIT="`git rev-parse --show-toplevel`"; then
+if TOPLEVEL_GIT="`git rev-parse --show-toplevel >/dev/null 2>&1`" ; then
   if [ -f $TOPLEVEL_GIT/.changelog-sh-conf ]; then
     export CHANGELOG_CONF_FILE=$TOPLEVEL_GIT/.changelog-sh-conf.sh
     
