af497ea441d248a8849d106a2c80f5d40f1ad651
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-20 13:43:57 +0200

    Don't install non tagged commits with install script or `change upgrade`

--- changelog-upgrade.sh
+++ changelog-upgrade.sh
@@ -6,5 +6,14 @@ function _changelogsh_upgrade {
   fi
 
   cd $CHANGE
-  git pull
+  # Code from https://stackoverflow.com/a/22857288/2256700 and https://stackoverflow.com/a/3867811/2256700
+  # Get new tags from remote
+  git fetch --tags
+
+  # Get latest tag name
+  latestTag=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD)
+
+  # Checkout latest tag
+  # Code from https://stackoverflow.com/a/45652159/2256700
+  git -c advice.detachedHead=false checkout $latestTag
 }
--- tools/install.sh
+++ tools/install.sh
@@ -15,7 +15,12 @@ git clone https://github.com/mricherzhagen/changelog-sh.git $CHANGE
 echo ""
 chmod +x $CHANGE/changelog.sh
 
-echo "To complete installation add an alias to your .bashrc file:"
-echo "\t alias change='sh $CHANGE/changelog.sh'"
+cd $CHANGE
+# Code from https://stackoverflow.com/a/3867811/2256700
+latestTag=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD)
+
+# Code from https://stackoverflow.com/a/45652159/2256700
+git -c advice.detachedHead=false checkout $latestTag
+
 echo ""
 echo "or add $CHANGE/bin to your \$PATH variable"
