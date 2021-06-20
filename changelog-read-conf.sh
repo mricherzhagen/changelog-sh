c5a00d08c8323117820e96bba07b98f143cf9718
mricherzhagen <mricherzhagen@users.noreply.github.com>
2021-06-20 20:36:40 +0200

    Fix missing prefix for `_execute_conf_file` function

--- changelog-read-conf.sh
+++ changelog-read-conf.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-function _execute_conf_file {
+function _changelogsh_execute_conf_file {
   source $1
   export CHANGELOGSH_HEADER
 }
 
 #Always read default config from $CHANGE
-_execute_conf_file $CHANGE/.changelog-sh-conf.sh
+_changelogsh_execute_conf_file $CHANGE/.changelog-sh-conf.sh
 if TOPLEVEL_GIT="`git rev-parse --show-toplevel >/dev/null 2>&1`" ; then
   CHANGELOGSH_INSIDE_GIT=true
   if [ -f $TOPLEVEL_GIT/.changelog-sh-conf ]; then
     export CHANGELOGSH_CONF_FILE=$TOPLEVEL_GIT/.changelog-sh-conf.sh
     
     #Overwrite defaults with per project settings
-    _execute_conf_file $CHANGELOGSH_CONF_FILE
+    _changelogsh_execute_conf_file $CHANGELOGSH_CONF_FILE
   fi
 else
   CHANGELOGSH_INSIDE_GIT=false
